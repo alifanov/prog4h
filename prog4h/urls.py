1c5b55069de741bbd9f0d6995dfbf8d9ec0a4d2a
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-03-11 17:13:46 +0400

    fix robokassa form

--- prog4h/urls.py
+++ prog4h/urls.py
@@ -37,6 +37,7 @@ urlpatterns = patterns('',
 
     url(r'^accounts/', include('registration.backends.default.urls')),
 
+    url(r'^robokassa/', include('robokassa.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
